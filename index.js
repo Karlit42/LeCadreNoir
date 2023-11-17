7df348c31f79a7c3d045838ef19e97aefc656d0d
Karlit <karlit@MBP-de-Karlit.lan>
2023-11-17 22:38:41 +0100

    commit initial

---
+++ index.js
@@ -0,0 +1,19 @@
+// // Définir la vitesse de défilement (en millisecondes)
+// const scrollSpeed = 20;
+
+// // Fonction de défilement automatique
+// function scrollDown() {
+//   // Vérifier si le bas de la page a été atteint
+//   if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+//     // Si c'est le cas, arrêter le défilement automatique
+//     clearInterval(scrollInterval);
+//   } else {
+//     // Sinon, faire défiler la page vers le bas
+//     window.scrollBy(0, 10);
+//   }
+// }
+
+// // Appeler la fonction de défilement automatique à intervalles réguliers
+// var scrollInterval = setInterval(scrollDown, scrollSpeed);
+
+ 
