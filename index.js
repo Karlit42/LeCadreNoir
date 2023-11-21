14dc167386a23288cc9562840df402271dbec927
Karlit <karlit@MBP-de-Karlit.lan>
2023-11-22 00:30:35 +0100

    mise en place d'une page d'intro

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// const page = document.getElementById('page');
+// // const intro = document.getElementById('intro');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   setTimeout(function () {
+//       page.classList.remove('hidden');
+//       // intro.classList.add('hidden');
+//   }, 2000);
+// });
+
 const redBtn = document.getElementById('red_mode');
 
 const cadre = document.querySelector('.cadre_noir');
@@ -6,6 +16,7 @@ if (localStorage.getItem('mode') === 'rouge') {
     cadre.classList.remove('cadre_noir');
     cadre.classList.add('cadre_rouge');
     redBtn.classList.add('redButton');
+    redBtn.innerHTML = 'Red to Black';
     }; 
 
 
@@ -15,11 +26,13 @@ redBtn.addEventListener('click', () => {
       cadre.classList.add('cadre_rouge');
       redBtn.classList.add('redButton');
       localStorage.setItem('mode', 'rouge');
+      redBtn.innerHTML = 'Red to Black';
     } else {
         cadre.classList.add('cadre_noir');
         cadre.classList.remove('cadre_rouge');
         redBtn.classList.remove('redButton');
         localStorage.setItem('mode', 'noir');
+        redBtn.innerHTML = 'Black to Red';
     }
   });
 
@@ -37,19 +50,25 @@ function masquerToutesLesSections() {
     accueilSection.classList.add('hidden');
     projetsSection.classList.add('hidden');
     contactSection.classList.add('hidden');
+    accueilBtn.innerHTML = 'Accueil';
+    projetsBtn.innerHTML = 'Projets';
+    contactBtn.innerHTML = 'Contact';
   };
 
 accueilBtn.addEventListener('click', function () {
     masquerToutesLesSections();
     accueilSection.classList.remove('hidden');
+    accueilBtn.innerHTML = '&#8861;';
 });
 
 projetsBtn.addEventListener('click', function () {
     masquerToutesLesSections();
     projetsSection.classList.remove('hidden');
+    projetsBtn.innerHTML = '&#8861;';
 });
 
 contactBtn.addEventListener('click', function () {
     masquerToutesLesSections();
     contactSection.classList.remove('hidden');
+    contactBtn.innerHTML = '&#8861;';
 });
